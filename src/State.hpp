9303b4949dcf07974aaa6411364de3982f47de84
KangSK-KAIST <steelyk7@gmail.com>
2021-01-15 01:21:24 +0900

    Added locking mechanism for safety + better comments

--- src/State.hpp
+++ src/State.hpp
@@ -17,28 +17,76 @@ private:
 	int iKey;
 	std::string sName;
 	outT (*pFunc)(inT);
+	bool isLocked = false;
 
 public:
 	State();
 	~State();
 	State operator()(inT input);
 
-	void setName(std::string name) { sName = name; };
-	void setFunction(outT (*func)(inT)) { pFunc = func; };
+	void setName(std::string name);
+	void setFunction(outT (*func)(inT));
 
 	outT giveInput(inT input){ return pFunc(input); };
 };
 
+
+/*
+ * Constructor of State class
+ * 
+ * A templated State class constructor.
+ * Template holds the input and output type.
+ */
 template <typename inT, typename outT>
 State<inT, outT>::State()
 {
 	std::cout << "Constructor" << std::endl;
 }
 
+/*
+ * Deconstructor of State class
+ * 
+ * A templated State class deconstructor.
+ * Template holds the input and output type.
+ */
 template <typename inT, typename outT>
 State<inT, outT>::~State()
 {
 	std::cout << "Deconstructor" << std::endl;
+}
+
+/*
+ * setName of State class
+ * 
+ * Checks whether the instance is locked and changes the name
+ * 
+ * @param std::string value of new name
+ * @return void / prints out error if locked
+ */
+template <typename inT, typename outT>
+State<inT, outT>::setName(std::string name)
+{
+	if (!isLocked)
+		sName = name;
+	else
+		std::cerr << "The state is locked. You should not modify the state after FSM running." << std::endl;
+}
+
+/*
+ * setFunction of State class
+ * 
+ * Checks whether the instance is locked and changes the function
+ * 
+ * @param function pointer of new function
+ * @return void / prints out error if locked
+ */
+template <typename inT, typename outT>
+State<inT, outT>::setFunction(outT (*func)(inT))
+{
+	if (!isLocked)
+		pFunc = func;
+	else
+		std::cerr << "The state is locked. You should not modify the state after FSM running." << std::endl;
 }
 
 #endif // INCLUDE_STATE_H_
