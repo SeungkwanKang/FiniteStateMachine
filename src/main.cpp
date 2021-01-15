e78afb2d22416c654b7c014c374b5774fdfef380
KangSK-KAIST <steelyk7@gmail.com>
2021-01-15 13:47:33 +0900

    Finished working example for State class

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,23 @@
 #include <string>
+#include <functional>
 #include "State.hpp"
 
 int main(int argc, char *argv[])
 {
 	State<int, std::string> s;
+
+	std::function<std::string(State<int, std::string>*, int)> f = \
+		[](State<int, std::string>* pThis, int input) {
+			if (input < 0)
+				std::cout << "under-zero: " << input << std::endl;
+			else
+				std::cout << "over-zero: " << input << std::endl;
+			return std::to_string(input);
+		};
+
+	s.setFunction(&f);
+
+	s.giveInput(3);
 
 	return 0;
 }
