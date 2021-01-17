441310193ba1049a3b588c8259fea227586de229
KangSK-KAIST <steelyk7@gmail.com>
2021-01-17 22:50:49 +0900

    Fixed FSM and State for Moore

--- src/FSM.hpp
+++ src/FSM.hpp
@@ -1,6 +1,8 @@
 #ifndef INCLUDE_FSM_H_
 #define INCLUDE_FSM_H_
 
+#define DEBUG_
+
 #include "State.hpp"
 
 template <typename inT, typename outT> class State;
@@ -15,16 +17,15 @@ class FSM
 	template <typename T1, typename T2>
 	friend class State_Mealy;
 
-private:
+protected:
 	bool isLocked;
 
-private:
+protected:
 	State<inT, outT>* pCurrState;
 	std::vector<State<inT, outT> *> vpState;
 
-private: // Setting the next state
+protected: // Setting the next state
 	void setNextStateByKey(int key);
-	void convertNameToKey(std::string name);
 	void setNextStateByName(std::string name);
 
 public: // Initialization related
@@ -32,8 +33,8 @@ public: // Initialization related
 	~FSM();
 	void addState(State<inT, outT> *state);
 
-public:
-	outT operator()(inT input);
+public: // Execution related
+	virtual outT operator()(inT input) = 0;
 };
 
 /*
@@ -69,6 +70,9 @@ FSM<inT, outT>::~FSM()
 /*
  * adds new State to FSM
  * 
+ * checks for locks (FSM and State)
+ * grabs start State if any
+ * 
  * @param pointer to the new State
  * @return void / throws error if locked
  */
@@ -80,37 +84,30 @@ void FSM<inT, outT>::addState(State<inT, outT>* pstate)
 		std::cerr << "FSM is locked. Do not modify FSM after it's running." << std::endl;
 		throw("[FSM] Modify after lock error");
 	}
-	if (pstate->iKey != -1)
-	{ // Addind same state again
+	if ((pstate->isLocked) || (pstate->iKey != -1))
+	{ // Adding same state again
 		std::cerr << "Adding same State multiple times." << std::endl;
 		throw("[FSM] Multiple addition of State instance");
+	}
+	if (pstate->sName == "")
+	{ // No name given
+		std::cerr << "No name given for State." << std::endl;
+		throw("[FMS] Received no-name State instance");
 	}
 
 	pstate->isLocked = true;
 	pstate->iKey = (int)vpState.size();
 	vpState.push_back(pstate);
 	if (pstate->isStartState)
-		pCurrState = pstate;
-}
-
-/*
- * sets the next State by key
- * 
- * the key value is the index in the State vector.
- * Check the index bound and change the current State
- * 
- * @param integer value of key
- * @return void / prints out error if out of bounds
- */
-template <typename inT, typename outT>
-outT FSM<inT, outT>::operator()(inT input)
-{
-	if (pCurrState == nullptr)
 	{
-		std::cerr << "There was no start State given. Add a start State." << std::endl;
-		throw("No start State error");
+		if (pCurrState == nullptr)
+			pCurrState = pstate;
+		else
+		{
+			std::cerr << "Received multiple start states." << std::endl;
+			throw("[FSM] Received multiple start states");
+		}
 	}
-	return pCurrState->giveInput(input);
 }
 
 /*
@@ -135,15 +132,17 @@ void FSM<inT, outT>::setNextStateByKey(int key)
  * sets the next State by name
  * 
  * the key value is the index in the State vector.
- * TODO
  * 
  * @param integer value of key
- * @return void / prints out error if out of bounds
+ * @return void / throws error if no such name
  */
 template <typename inT, typename outT>
-void FSM<inT, outT>::convertNameToKey(std::string name)
+void FSM<inT, outT>::setNextStateByName(std::string name)
 {
-	for (size_t key = 0; key < vpState.length(); key++)
+#ifdef DEBUG_
+	std::cout << "[FSM] setNextStateByName called: " << name << std::endl;
+#endif
+	for (size_t key = 0; key < vpState.size(); key++)
 	{
 		if (name == vpState[key]->sName)
 		{
@@ -151,12 +150,7 @@ void FSM<inT, outT>::convertNameToKey(std::string name)
 		}
 	}
 	std::cerr << "There is no State with such name." << std::endl;
-}
-
-template <typename inT, typename outT>
-void FSM<inT, outT>::setNextStateByName(std::string name)
-{
-	return; // TODO
+	throw("[FSM] Non-valid name for State");
 }
 
 #endif // INCLUDE_FSM_H_
--- src/State.hpp
+++ src/State.hpp
@@ -1,6 +1,8 @@
 #ifndef INCLUDE_STATE_H_
 #define INCLUDE_STATE_H_
 
+#define DEBUG_
+
 #include <iostream>
 #include <string>
 #include <vector>
@@ -20,29 +22,31 @@ class State
 	template <typename T1, typename T2>
 	friend class FSM_Mealy;
 
-private: // Internals
+protected: // Internals
 	State *pParent;
 
-private: // Identification related
+protected: // Identification related
 	int iKey;
 	std::string sName;
 	bool isStartState;
+	bool isEndState;
 
-private: // Function related
-	std::function<std::string(State<int, std::string> *, int)> *pFunc;
+protected: // Function related
+	// Transition function
+	std::function<std::string(inT)> *pTransFunc;
 	bool isLocked = false;
 
-private: // Input related
-	outT giveInput(inT input);
-	void callStateByKey(int key);
+protected: // Input related
+	virtual outT getOutput(inT input) = 0;
 	void callStateByName(std::string name);
 
 public: // Initialization related
 	State();
 	~State();
 	void setName(std::string name);
-	void setFunction(std::function<std::string(State<int, std::string> *, int)> *);
+	void setTransitionFunction(std::function<std::string(inT)> *);
 	void setAsStartState();
+	void setAsEndState();
 };
 
 
@@ -60,8 +64,8 @@ State<inT, outT>::State()
 #endif
 	iKey = -1;
 	sName = "";
+	pTransFunc = nullptr;
 	isStartState = false;
-	std::cout << "Constructor" << std::endl;
 }
 
 /*
@@ -89,6 +93,9 @@ State<inT, outT>::~State()
 template <typename inT, typename outT>
 void State<inT, outT>::setName(std::string name)
 {
+#ifdef DEBUG_
+	std::cout << "[State] setName called: " << name << std::endl;
+#endif
 	if (!isLocked)
 		sName = name;
 	else
@@ -96,20 +103,23 @@ void State<inT, outT>::setName(std::string name)
 }
 
 /*
- * setFunction of State class
+ * setTransitionFunction of State class
  * 
- * Checks whether the instance is locked and changes the function
+ * Checks whether the instance is locked and changes the transition function
  * 
- * @param function pointer of new function
- * @return void / prints out error if locked
+ * @param function pointer of new transition function
+ * @return void / throws error if locked
  */
 template <typename inT, typename outT>
-void State<inT, outT>::setFunction(std::function<std::string(State<int, std::string> *, int)> *func)
+void State<inT, outT>::setTransitionFunction(std::function<std::string(inT)> *func)
 {
 	if (!isLocked)
-		pFunc = func;
+		pTransFunc = func;
 	else
+	{
 		std::cerr << "The state is locked. You should not modify the state after FSM running." << std::endl;
+		throw("[State] Modify after lock error");
+	}
 }
 
 template <typename inT, typename outT>
@@ -119,27 +129,9 @@ void State<inT, outT>::setAsStartState()
 }
 
 template <typename inT, typename outT>
-outT State<inT, outT>::giveInput(inT input)
+void State<inT, outT>::setAsEndState()
 {
-	return pFunc->operator()(this, input);
-};
-
-/*
- * calls the next state by key
- * 
- * Checks whether the parent (FSM) is set and modifies its next state
- * 
- * @param integer value of key
- * @return void / prints out error if parent not set
- */
-template <typename inT, typename outT>
-void State<inT, outT>::callStateByKey(int key)
-{
-	if (pParent != NULL)
-		pParent->setNextStateByKey(key);
-	else
-		std::cerr << "Please load the state first." << std::endl;
-	
+	isEndState = true;
 }
 
 /*
@@ -148,7 +140,7 @@ void State<inT, outT>::callStateByKey(int key)
  * Checks whether the parent (FSM) is set and modifies its next state
  * 
  * @param std::string value of name
- * @return void / prints out error if parent not set
+ * @return void / throws error if parent not set
  */
 template <typename inT, typename outT>
 void State<inT, outT>::callStateByName(std::string name)
@@ -157,7 +149,10 @@ void State<inT, outT>::callStateByName(std::string name)
 		if (pParent != NULL)
 			pParent->setNextStateByName(name);
 		else
+		{
 			std::cerr << "Please load the state first." << std::endl;
+			throw("[State] Call before set error");
+		}
 	}
 }
 
